4d95a8db51b96adc88bb9ca9c960393fb5cd844d
Adrian Fuhrmann <adrian@giantmonkey.de>
2014-12-22 14:19:08 +0100

    Revert "Removed rails streaming due to bugs with threads. Added mime-type detection."
    This reverts commit 536ce79e2e37ee134b38c77fce47dc41f1ea39e7.

--- app/controllers/postgresql_lo_streamer/lo_controller.rb
+++ app/controllers/postgresql_lo_streamer/lo_controller.rb
@@ -3,21 +3,22 @@
 module PostgresqlLoStreamer
   class LoController < ActionController::Base
     def stream
-      mime_type = Mime::Type.lookup_by_extension(request.original_url.split('.').last)
-      ActiveRecord::Base.transaction do
-        lo = lo_manager.java_send :open, [Java::long, Java::int], params[:id].to_i, Java::OrgPostgresqlLargeobject::LargeObjectManager::READ
-        data = lo.read(lo.size)
-        send_data data.to_s, type: mime_type, disposition: 'inline'
+      # TODO: make this headers configurable
+      send_file_headers!({:type => 'image/png', :disposition => 'inline'})
+      self.status = 200
+      self.response_body = Enumerator.new do |y|
+        connection.transaction do
+          lo = connection.lo_open(params[:id].to_i, ::PG::INV_READ)
+          while data = connection.lo_read(lo, 4096) do
+            y << data
+          end
+          connection.lo_close(lo)
+        end
       end
     end
 
-    private
     def connection
       @con ||= ActiveRecord::Base.connection.raw_connection
-    end
-
-    def lo_manager
-      @lo_manager ||= connection.connection.getLargeObjectAPI
     end
   end
 end
